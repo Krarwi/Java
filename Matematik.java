f655312967c194a4db843800f3da59bdf1d9f14e
unknown <glimmergo0se@gmail.com>
2024-08-26 13:39:36 +0300

    added math

---
+++ Matematik.java
@@ -0,0 +1,11 @@
+public class Matematik {
+
+    public static void main(String args[]) {
+        int num1 = 30;
+        int num2 = 4;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+    }
+}
