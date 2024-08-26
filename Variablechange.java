c8c5b451abf77b2c83e795a6cec3b8ee90be268a
unknown <glimmergo0se@gmail.com>
2024-08-26 13:58:46 +0300

    variable change stuff

---
+++ Variablechange.java
@@ -0,0 +1,22 @@
+public class Variablechange {
+
+    public static void main(String args[]) {
+        int number = 41;
+        float number2 = (float) 6.54;
+        double number3 = 123;
+        char number4 = 2;
+        int number5 = 58;
+        
+        System.out.println(number);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(number5); 
+        
+        System.out.println((double)number);
+        System.out.println((double)number2);
+        System.out.println((int)number3);
+        System.out.println((int)number4);
+        System.out.println((byte)number5); 
+    }
+}
