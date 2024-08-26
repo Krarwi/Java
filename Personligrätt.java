ad70e2061ce340816a0e0c58572f376726456546
unknown <glimmergo0se@gmail.com>
2024-08-26 13:32:58 +0300

    Adding shit

---
+++ Personligrätt.java
@@ -0,0 +1,17 @@
+public class Personligrätt {
+
+    public static void main(String args[]) {
+        
+        String Firstname = "Pedro";
+        String Lastname = "Gonzalez";
+        int Age = 35;
+        double Height = 167;
+        boolean Student = false;
+        
+        System.out.println("First name: " + Firstname);
+        System.out.println("Last name: " + Lastname);
+        System.out.println("Age: " + Age);
+        System.out.println("Height: " + Height);
+        System.out.println("Student: " + Student);
+    }
+}
