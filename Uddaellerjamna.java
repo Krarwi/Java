9f160328489a3c45386ea4e6715b9b471251eabb
unknown <glimmergo0se@gmail.com>
2024-08-27 12:06:10 +0300

    udda eller jämn

---
+++ Uddaellerjamna.java
@@ -0,0 +1,13 @@
+
+public class Uddaellerjamna {
+
+
+    public static void main(String args[]) {
+       int tal = 2;
+       
+        if(tal % 0 == 2)
+            System.out.println(tal + " är jämn");
+        else
+            System.out.println(tal + " är udda");
+    }
+}
