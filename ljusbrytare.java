f5445e9d364b2254b5a0ae8141c8a8f534c95664
unknown <glimmergo0se@gmail.com>
2024-08-27 13:04:30 +0300

    ljustbrytare

---
+++ ljusbrytare.java
@@ -0,0 +1,16 @@
+
+public class ljusbrytare {
+
+
+    public static void main(String args[]) {
+        
+        boolean ljus = false;
+        if (ljus == false) {
+            System.out.println("Ljusen är av");
+        }
+        else if (ljus == true) {
+            System.out.println("Ljusen är på");
+        }
+            
+    }
+}
