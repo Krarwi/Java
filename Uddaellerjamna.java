2a9c8a78f423c8eb894f9084030b75191a7aab87
Krarwi <glimmergo0se@gmail.com>
2024-09-03 12:30:45 +0300

    Update Uddaellerjamna.java

--- Uddaellerjamna.java
+++ Uddaellerjamna.java
@@ -5,9 +5,9 @@ public class Uddaellerjamna {
     public static void main(String args[]) {
        int tal = 2;
        
-        if(tal % 0 == 2)
-            System.out.println(tal + " är jämn");
+        if(tal % 2 == 0)
+            System.out.println(tal + " even");
         else
-            System.out.println(tal + " är udda");
+            System.out.println(tal + " odd");
     }
 }
